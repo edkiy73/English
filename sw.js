0f3e650a67394503118737a5699742dc1539aa4e
edkiy73 <edkiy73@gmail.com>
2026-09-04 18:32:37 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* English Trainer — офлайн-кэш.
    Меняй CACHE при каждом обновлении файлов. */
-const CACHE = "english-trainer-v16";
+const CACHE = "english-trainer-v17";
 const ASSETS = [
   "./",
   "./index.html",
